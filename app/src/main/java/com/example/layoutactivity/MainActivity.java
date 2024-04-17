78ac59141b5777739699ccfa9e9858b2785c519f
Giles Anthony Villamor II <gilesanthony.villamorii@cit.edu>
2024-04-17 14:58:36 +0800

    added passingIntents and additional activity buttons

--- app/src/main/java/com/example/layoutactivity/MainActivity.java
+++ app/src/main/java/com/example/layoutactivity/MainActivity.java
@@ -27,8 +27,13 @@ public class MainActivity extends AppCompatActivity {
             startActivity(openActivity);
         });
 
-        findViewById(R.id.connect3Btn).setOnClickListener(view -> {
+        findViewById(R.id.connectMatch3Btn).setOnClickListener(view -> {
             Intent openActivity = new Intent(MainActivity.this, Connect3.class);
+            startActivity(openActivity);
+        });
+
+        findViewById(R.id.passingIntentsActivityBtn).setOnClickListener(view -> {
+            Intent openActivity = new Intent(MainActivity.this, PassingIntentsExercise.class);
             startActivity(openActivity);
         });
     }
---
+++ app/src/main/java/com/example/layoutactivity/PassingIntentsExercise2.java
@@ -0,0 +1,14 @@
+package com.example.layoutactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PassingIntentsExercise2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_passing_intents_exercise2);
+    }
+}
